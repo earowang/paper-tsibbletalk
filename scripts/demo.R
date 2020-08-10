87aa0d122a40212bedb354336d768f99d1bf46b5
Earo Wang <earo.wang@gmail.com>
2020-08-10 14:20:28 +1200

    reworked on tidyverts intro

---
+++ scripts/demo.R
@@ -0,0 +1,38 @@
+## ---- load-pkgs
+library(feasts)
+library(tsibble)
+library(tsibbledata)
+
+## ---- print-retail
+print(aus_retail, n = 5)
+
+## ---- highlight-retail
+library(tidyverse)
+library(ggrepel)
+library(gghighlight)
+library(patchwork)
+
+p_retail_ts <- aus_retail %>% 
+  as_tibble() %>% # gghighlight issue for group_by() + filter()
+  mutate(group = paste(State, ":", Industry)) %>% 
+  ggplot(aes(x = Month, y = Turnover)) +
+  geom_line(aes(group = group)) +
+  gghighlight(
+    State == "Queensland", Industry == "Department stores",
+    label_params = list(vjust = -2)
+  ) +
+  scale_x_yearmonth(breaks = yearmonth(c("1990 Jan", "2000 Jan", "2010 Jan")))
+
+p_retail_feat <- aus_retail %>% 
+  features(Turnover, feat_stl) %>% 
+  mutate(group = paste(State, ":", Industry)) %>% 
+  ggplot(aes(x = trend_strength, y = seasonal_strength_year)) +
+  geom_point(aes(group = group)) +
+  labs(x = "Trend strength", y = "Seasonal strength") +
+  gghighlight(
+    State == "Queensland", Industry == "Department stores",
+    use_direct_label = FALSE
+  ) +
+  geom_label_repel(aes(label = group), vjust = 2, nudge_x = -0.1)
+
+p_retail_ts + p_retail_feat + plot_annotation(tag_levels = "a")
