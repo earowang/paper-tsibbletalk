520978e685fb7ad4e6be92a5bd81a8f4368f16d0
earowang <earo.wang@gmail.com>
2020-08-25 16:30:18 +1200

    more on figure 2

--- scripts/tourism.R
+++ scripts/tourism.R
@@ -10,23 +10,24 @@ library(ggplot2)
 library(tsibble)
 
 ## ---- plotly-key-tree
+p_l <- plotly_key_tree(tourism_shared, height = 1100, width = 800)
+
+## ---- tourism-linking
 tourism_feat <- tourism_shared %>%
   features(Trips, feat_stl)
-
-p0 <- plotly_key_tree(tourism_shared, height = 1100, width = 800)
-p1 <- tourism_shared %>%
+p_tr <- tourism_shared %>%
   ggplot(aes(x = Month, y = Trips)) +
   geom_line(aes(group = Region), alpha = .5, size = .4) +
   facet_wrap(~ Purpose, scales = "free_y") +
   scale_x_yearmonth(date_breaks = "5 years", date_labels = "%Y")
-p2 <- tourism_feat %>%
+p_br <- tourism_feat %>%
   ggplot(aes(x = trend_strength, y = seasonal_strength_year)) +
   geom_point(aes(group = Region))
 
-subplot(p0,
+subplot(p_l,
   subplot(
-    ggplotly(p1, tooltip = "Region", width = 1100),
-    ggplotly(p2, tooltip = "Region", width = 1100),
+    ggplotly(p_tr, tooltip = "Region", width = 1100),
+    ggplotly(p_br, tooltip = "Region", width = 1100),
     nrows = 2),
   widths = c(.4, .6)) %>%
   highlight(dynamic = TRUE)
