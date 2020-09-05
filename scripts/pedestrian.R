68b59efa41297b6fd20c110e5117c5efd1f0853a
earowang <earo.wang@gmail.com>
2020-09-05 16:20:47 +1200

    figure 3 descriptions

--- scripts/pedestrian.R
+++ scripts/pedestrian.R
@@ -1,3 +1,4 @@
+## ---- load-ped
 library(rwalkr)
 library(tsibble)
 library(tsibbletalk)
@@ -9,12 +10,13 @@ library(lubridate)
 sensors <- c("Birrarung Marr", "Bourke Street Mall (North)",
   "QV Market-Elizabeth St (West)", "Southern Cross Station")
 
-ped20 <- melb_walk_fast(year = 2020, sensor = sensors) %>% 
+pedestrian20 <- melb_walk_fast(year = 2020, sensor = sensors) %>% 
   filter(Date < ymd("2020-06-01")) %>% 
   mutate(Lockdown = ifelse(Date > ymd("2020-03-16"), "Yes", "No")) %>% 
   as_tsibble(index = Date_Time, key = Sensor)
 
-p_line <- ped20 %>%
+## ---- ped-slice
+p_line <- pedestrian20 %>%
   ggplot(aes(x = Date_Time, y = Count, colour = Lockdown)) +
   geom_line(size = .3) +
   facet_wrap(~ Sensor, scales = "free_y") +
@@ -22,12 +24,8 @@ p_line <- ped20 %>%
   scale_colour_brewer(palette = "Dark2") +
   theme(legend.position = "none")
 
-ui <- fluidPage(
-  tsibbleDiceUI("dice")
-)
-
+ui <- fluidPage(tsibbleDiceUI("dice"))
 server <- function(input, output, session) {
   tsibbleDiceServer("dice", ggplotly(p_line, height = 700), period = "1 day")
 }
-
 shinyApp(ui, server)
